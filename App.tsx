9f15acc102c2510d40a982ff89781f5ff8592884
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-08-12 21:20:10 -0300

    Create details screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState} from 'react';
+import { ThemeProvider } from 'styled-components'
+
+import {
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+import { CarDetails } from './src/screens/CarDetails';
+
+import theme from './src/styles/theme';
+
+import { View } from 'react-native';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium,
+          Archivo_400Regular,
+          Archivo_500Medium,
+          Archivo_600SemiBold
+        })
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if(appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if(!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Rentx!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View
+        onLayout={onLayoutRootView}
+        style={{
+          flex: 1
+        }}  
+      >
+        <CarDetails />
+      </View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

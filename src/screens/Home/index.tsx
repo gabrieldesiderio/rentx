ae31a8e1e3d3e168c1162e48e61d957b03c30cba
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-09-02 18:29:22 -0300

    create api service

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,61 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useNavigation } from "@react-navigation/native";
 
 import Logo from "../../assets/logo.svg";
+import { api } from "../../services/api";
+import { CarDTO } from "../../dtos/CarDTO";
 
-import { Car } from '../../components/Car';
+import { Car } from "../../components/Car";
+import { Load } from "../../components/Load";
 
-import { 
-  Container, 
-  Header, 
-  HeaderContent,
-  TotalCars,
-  CarList
-} from "./styles";
+import { Container, Header, HeaderContent, TotalCars, CarList } from "./styles";
 
 export function Home() {
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
 
   const carData = {
-    brand: 'audi',
-    name: 'RS 5 Coupé',
+    brand: "audi",
+    name: "RS 5 Coupé",
     rent: {
-      period: 'Ao dia',
+      period: "Ao dia",
       price: 120,
     },
-    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+    thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
   };
 
   function handleCarDetails() {
-    navigation.navigate('CarDetails');
+    navigation.navigate("CarDetails");
   }
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const { data } = await api.get("/cars");
+        setCars(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCars();
+  }, []);
 
   return (
     <Container>
-      <StatusBar 
-        style="light" 
-        backgroundColor="transparent" 
-        translucent 
-      />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
 
       <Header>
         <HeaderContent>
-          <Logo 
-            width={RFValue(108)} 
-            height={RFValue(12)} 
-          />
+          <Logo width={RFValue(108)} height={RFValue(12)} />
 
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
 
-      <CarList 
-        data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-        keyExtractor={item => String(item)}
-        renderItem={({ item }) => <Car data={carData} onPress={handleCarDetails} />}
-      />
+      {loading ? (
+        <Load />
+      ) : (
+        <CarList
+          data={cars}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Car data={item} onPress={handleCarDetails} />
+          )}
+        />
+      )}
     </Container>
   );
 }

2118f2dca2607c6f1c9bf95410e94b0a57fbe20b
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-09-22 21:40:12 -0300

    remove action button back from home screen on android

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { StatusBar, StyleSheet } from "react-native";
+import { StatusBar, StyleSheet, BackHandler } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { RFValue } from "react-native-responsive-fontsize";
 
@@ -75,6 +75,12 @@ export function Home() {
     }
 
     fetchCars();
+  }, []);
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      return true;
+    })
   }, []);
 
   return (

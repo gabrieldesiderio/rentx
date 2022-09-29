655ef26d338439abf0bebdd195b28bf7fead399d
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-09-29 19:44:00 -0300

    form validation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,7 +4,10 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
+import * as Yup from 'yup'
+
 import { useTheme } from "styled-components";
 
 import { Button } from "../../components/Button";
@@ -18,6 +21,30 @@ export function SignIn() {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string()
+          .required('A senha é obrigatória')
+      });
+  
+      await schema.validate({ email, password });
+      Alert.alert('Tudo certo!');
+    } catch (error) {
+      if(error instanceof Yup.ValidationError) {
+        Alert.alert('Falha na validação', error.message);
+      } else {
+        Alert.alert(
+          'Erro na autenticação', 
+          'Ocorreu um erro ao fazer login. Verifique as credenciais'
+        );
+      }
+    }
+  }
 
   return (
     <KeyboardAvoidingView behavior="position" enabled>
@@ -61,8 +88,8 @@ export function SignIn() {
           <S.Footer>
             <Button
               title="Login"
-              onPress={() => {}}
-              enabled={false}
+              onPress={handleSignIn}
+              enabled={true}
               loading={false}
             />
             <Button

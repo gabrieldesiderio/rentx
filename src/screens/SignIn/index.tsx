4b25ecfcec2597bbe9e857f07a1f1be3f36ed8da
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-09-29 15:20:46 -0300

    form accessibility

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,65 +1,71 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
-import { useTheme } from 'styled-components';
+import React from "react";
+import {
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useTheme } from "styled-components";
 
-import { Button } from '../../components/Button';
-import { Input } from '../../components/Input';
-import { PasswordInput } from '../../components/PasswordInput';
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { PasswordInput } from "../../components/PasswordInput";
 
-import * as S from './styles';
+import * as S from "./styles";
 
 export function SignIn() {
   const theme = useTheme();
 
   return (
-    <S.Container>
-      <StatusBar 
-        barStyle='dark-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      <S.Header>
-        <S.Title>
-          Estamos{'\n'}
-          quase lá.
-        </S.Title>
-        <S.Subtitle>
-          Faça seu login para começar{'\n'}
-          uma experiência incrível.
-        </S.Subtitle>
-      </S.Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <S.Container>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <S.Header>
+            <S.Title>
+              Estamos{"\n"}
+              quase lá.
+            </S.Title>
+            <S.Subtitle>
+              Faça seu login para começar{"\n"}
+              uma experiência incrível.
+            </S.Subtitle>
+          </S.Header>
 
-      <S.Form>
-        <Input 
-          iconName="mail" 
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
+          <S.Form>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
 
-        <PasswordInput 
-          iconName="lock"
-          placeholder="Senha"
-        />
-      </S.Form>
+            <PasswordInput iconName="lock" placeholder="Senha" />
+          </S.Form>
 
-      <S.Footer>
-        <Button 
-          title="Login"
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-        <Button 
-          title="Criar conta gratuita"
-          onPress={() => {}}
-          light
-          enabled={false}
-          loading={false}
-          color={theme.colors.background_secondary}
-        />
-      </S.Footer>
-    </S.Container>
+          <S.Footer>
+            <Button
+              title="Login"
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
+            <Button
+              title="Criar conta gratuita"
+              onPress={() => {}}
+              light
+              enabled={false}
+              loading={false}
+              color={theme.colors.background_secondary}
+            />
+          </S.Footer>
+        </S.Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -3,7 +3,6 @@ import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-  flex: 1;
   padding: 0 24px;
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
@@ -14,14 +13,14 @@ export const Header = styled.View`
 `;
 
 export const Title = styled.Text`
-  font-size: ${RFValue(40)};
+  font-size: ${RFValue(40)}px;
   font-family: ${({ theme }) => theme.fonts.secondary_600};
   color: ${({ theme }) => theme.colors.title};
 `;
 
 export const Subtitle = styled.Text`
-  font-size: ${RFValue(15)};
-  line-height: ${RFValue(25)};
+  font-size: ${RFValue(15)}px;
+  line-height: ${RFValue(25)}px;
   font-family: ${({ theme }) => theme.fonts.primary_400};
   color: ${({ theme }) => theme.colors.text};
   margin-top: 16px;

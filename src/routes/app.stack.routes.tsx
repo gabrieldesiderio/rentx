6ed1f9b925498e5859f31967863612f39486c4f2
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-10-06 17:01:24 -0300

    auth routes

---
+++ src/routes/app.stack.routes.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Home } from "../screens/Home";
+import { CarDetails } from "../screens/CarDetails";
+import { Scheduling } from "../screens/Scheduling";
+import { Confirmation } from "../screens/Confirmation";
+import { SchedulingDetails } from "../screens/SchedulingDetails";
+import { MyCars } from "../screens/MyCars";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppStackRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+      <Screen name="Home" component={Home} />
+      <Screen name="CarDetails" component={CarDetails} />
+      <Screen name="Scheduling" component={Scheduling} />
+      <Screen name="Confirmation" component={Confirmation} />
+      <Screen name="SchedulingDetails" component={SchedulingDetails} />
+      <Screen name="MyCars" component={MyCars} />
+    </Navigator>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-import { StatusBar, StyleSheet, BackHandler } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
-import { RectButton, PanGestureHandler } from "react-native-gesture-handler";
-
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  useAnimatedGestureHandler
-} from "react-native-reanimated";
-
-const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
+import { useNavigation } from "@react-navigation/native";
 
 import Logo from "../../assets/logo.svg";
 import { api } from "../../services/api";
@@ -22,44 +12,15 @@ import { Car } from "../../components/Car";
 import { LoadAnimation } from "../../components/LoadAnimation";
 
 import { Container, Header, HeaderContent, TotalCars, CarList } from "./styles";
-import { useTheme } from "styled-components";
 
 export function Home() {
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
 
-  const positionY = useSharedValue(0);
-  const positionX = useSharedValue(0);
-
-  const myCarsButtonStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: positionX.value },
-        { translateY: positionY.value },
-      ],
-    };
-  });
-
-  const onGestureEvent = useAnimatedGestureHandler({
-    onStart(_, ctx: any) {
-      ctx.positionX = positionX.value;
-      ctx.positionY = positionY.value;
-    },
-    onActive(event, ctx: any) {
-      positionX.value = ctx.positionX + event.translationX;
-      positionY.value = ctx.positionY + event.translationY;
-    }
-  });
-
   const navigation = useNavigation();
-  const theme = useTheme();
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate("CarDetails", { car });
-  }
-
-  function handleOpenMyCars() {
-    navigation.navigate("MyCars");
   }
 
   useEffect(() => {
@@ -77,12 +38,6 @@ export function Home() {
     fetchCars();
   }, []);
 
-  useEffect(() => {
-    BackHandler.addEventListener('hardwareBackPress', () => {
-      return true;
-    })
-  }, []);
-
   return (
     <Container>
       <StatusBar style="light" backgroundColor="transparent" translucent />
@@ -91,11 +46,7 @@ export function Home() {
         <HeaderContent>
           <Logo width={RFValue(108)} height={RFValue(12)} />
 
-          {
-            !loading && (
-              <TotalCars>Total de {cars.length} carros</TotalCars>
-            )
-          }
+          {!loading && <TotalCars>Total de {cars.length} carros</TotalCars>}
         </HeaderContent>
       </Header>
 
@@ -110,40 +61,6 @@ export function Home() {
           )}
         />
       )}
-
-      <PanGestureHandler onGestureEvent={onGestureEvent} >
-        <Animated.View
-          style={[
-            myCarsButtonStyle,
-            {
-              position: "absolute",
-              bottom: 13,
-              right: 22,
-            },
-          ]}
-        >
-          <ButtonAnimated
-            onPress={handleOpenMyCars}
-            style={[styles.button, { backgroundColor: theme.colors.main }]}
-          >
-            <Ionicons
-              name="ios-car-sport"
-              size={32}
-              color={theme.colors.shape}
-            />
-          </ButtonAnimated>
-        </Animated.View>
-      </PanGestureHandler>
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  button: {
-    height: 60,
-    width: 60,
-    borderRadius: 30,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});

45c449fd1ac77f0e79a6a136d526c9930ed52871
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-10-06 15:43:30 -0300

    refact api DTO

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -29,7 +29,7 @@ const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 export function StackRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }} initialRouteName="SignIn">
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
       <Screen name="SignIn" component={SignIn} />
       <Screen name="SignUpFirstStep" component={SignUpFirstStep} />
       <Screen name="SignUpSecondStep" component={SignUpSecondStep} />
